9ee1f7cd6b74d5cc44ea6a08c764858000873113
TUAS-FIT <turkugamelabdev@gmail.com>
2023-08-11 10:34:44 +0000

    Update from https://github.com/TUAS-FIT/npm-client/commit/703e9efb6acbe796545958d775e76e047b110177

--- Editor/NPMClientWindow.cs
+++ Editor/NPMClientWindow.cs
@@ -24,10 +24,10 @@ namespace NPMClient.Editor
         private const float showPasswordButtonWidth = 50f;
         private const float loginButtonWidth = 60f;
         
-        private const string registryURL   = "http://35.228.175.216:4873/";
+        private const string registryURL   = "https://npm.dc.turkuamk.fi/";
         private const string registryName  = "TUAS-FIT";
         private const string registryScope = "com.tuas-fit";
-        private const string shortenRegistryURL  = "35.228.175.216:4873";
+        private const string shortenRegistryURL  = "npm.dc.turkuamk.fi";
         private const string loginExecutableName = "npm-login";
         private const string jsonFileName = "npm-login.json";
 
